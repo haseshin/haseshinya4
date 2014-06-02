5f9816697a047530f1ad4d6645588dd9200793d5
nssv-04 <nssv-04@192.168.1.5>
2014-06-02 16:53:27 +0900

    あと筒香じゃなくする処理を記載する

---
+++ KaLists/src/com/kaneda/KaLists/DatabaseHelper.java
@@ -0,0 +1,76 @@
+package com.kaneda.KaLists;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * 野球選手名鑑
+ * データベースのヘルパークラス
+ *
+ * @author kaneda
+ */
+public class DatabaseHelper extends SQLiteOpenHelper{
+	/**
+	 * データベースファイル名の定数フィールド
+	 */
+	private static final String DATABASE_NAME = "baseball.db";
+	/**
+	 * バージョン情報の定数フィールド
+	 */
+	private static final int DATABASE_VERSION =1;
+	private static Context mContext = null;
+
+	/**
+	 * コンストラクタ
+	 * @param context コンテキスト
+	 */
+	public DatabaseHelper(Context context) {
+		super(context,DATABASE_NAME,null,DATABASE_VERSION);
+		this.mContext=context;
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase db){
+
+		//トランザクションの開始
+		db.beginTransaction();
+
+		//クリエイト文の作成
+		StringBuffer sb = new StringBuffer();
+
+		//create文/insert文の取得
+		InputStream is;
+	    BufferedReader bfReader;
+        try {
+            is = mContext.getAssets().open("player.sql");
+            bfReader = new BufferedReader(new InputStreamReader(is));
+            String line;
+            while(( line = bfReader.readLine() ) != null){
+                if(! line.equals("") ){
+                    sb.append(line);
+                    sb.append("\n");
+                }
+            }
+            sb.deleteCharAt(sb.length()-1);
+            for(String sql: sb.toString().split(";")){
+                db.execSQL(sql);
+            }
+        } catch (IOException e) {
+             e.printStackTrace();
+    }
+		//成功したとき
+		db.setTransactionSuccessful();
+		//トランザクションの終了
+		db.endTransaction();
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db,int oldVersion,int newVersion){
+	}
+}
---
+++ KaLists/src/com/kaneda/KaLists/TempleListActivity.java
@@ -0,0 +1,67 @@
+package com.kaneda.KaLists;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+/**
+ * SA41 Androidサンプル　Chapter04 Ex01 データベース
+ *
+ * 第一画面表示用アクティビティクラス
+ * お寺リストを表示する *
+ * @author ohs05032
+*/
+
+public class TempleListActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_temple_list);
+
+		ListView lvTemple = (ListView) findViewById(R.id.lv_temple);
+
+		List<String> templeList = createPrefectureList();
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,templeList);
+
+		lvTemple.setAdapter(adapter);
+		lvTemple.setOnItemClickListener(new  ListItemClickListener());
+	}
+
+
+	/**
+	 *球団を生成するメソッド
+	 * @return 球団リストオブジェクト
+	 */
+	private  List<String> createPrefectureList(){
+				List<String> groupList = new ArrayList<String>();
+				groupList.add("阪神タイガース");
+				groupList.add("読売ジャイアンツ");
+				return  groupList;
+	}
+	/**
+	 * リストが選択されたときの処理が
+	 * 第2画面へ処理を移管する
+	 *
+	 * @author kaneda
+	 */
+
+	private class ListItemClickListener implements OnItemClickListener{
+		@Override
+		public void onItemClick(AdapterView<?>  parent, View view, int position,long id){
+			ListView listView = (ListView) parent;
+			String group = (String) listView.getItemAtPosition(position);
+
+			Intent intent = new Intent(TempleListActivity.this,SecondListActivity.class);
+			intent.putExtra("selectedgroup", group);
+			startActivity(intent);
+		}
+	}
+}
